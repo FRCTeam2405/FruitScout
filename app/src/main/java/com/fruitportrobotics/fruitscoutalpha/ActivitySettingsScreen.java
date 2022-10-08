234b2a33179b836919b3cd6f9c1f11cb82193207
Moostashgaming <ethanmartinez2008@gmail.com>
2022-10-08 11:41:03 -0400

    Fixed: Android Status Bar no longer visible Ongoing: Many layouts still unfinished, Layout needs to be resized for the Kindle (Current Size: 1280x800 dp) Note:

--- app/src/main/java/com/fruitportrobotics/fruitscoutalpha/ActivitySettingsScreen.java
+++ app/src/main/java/com/fruitportrobotics/fruitscoutalpha/ActivitySettingsScreen.java
@@ -6,18 +6,18 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class ActivitySettingsScreen extends AppCompatActivity {
+public class ActivitySettingsScreen extends ActivityTitleScreen {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_screen);
     }
-//    // Switch Activity Method (Creates a switchActiveIntent Intent and starts the activity referenced )
-//    public void switchActivity(Class switchTo) {
-//        Intent switchActivityIntent = new Intent(this, switchTo);
-//        startActivity(switchActivityIntent);
-//        // May cause problems
-//        this.finish();
-//    }
+    // Switch Activity Method (Creates a switchActiveIntent Intent and starts the activity referenced )
+    public void switchActivity(Class switchTo) {
+        Intent switchActivityIntent = new Intent(this, switchTo);
+        startActivity(switchActivityIntent);
+        // May cause problems
+        this.finish();
+    }
 }
